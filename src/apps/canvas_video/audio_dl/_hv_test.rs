1b8ea746dc4cdd27f17f7dfe8e1668f210643881
unknown <wuyutanhongyuxin@gmail.com>
2026-05-10 21:09:43 +0800

    chore: remove debug leftover _hv_test.rs

--- src/apps/canvas_video/audio_dl/_hv_test.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let r = reqwest::header::HeaderValue::from_str("https://例.cn");
-    println!("result: {:?}", r);
-    let r2 = reqwest::header::HeaderValue::from_str("https://courses.sjtu.edu.cn");
-    println!("result2: {:?}", r2);
-}
